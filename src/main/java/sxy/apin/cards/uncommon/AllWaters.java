adbe155220695ec59c86cdacba7ef2f3517a0b39
Apin <qjksxy@163.com>
2024-09-14 00:51:25 +0800

    :pencil: 更新卡牌列表

--- src/main/java/sxy/apin/cards/uncommon/AllWaters.java
+++ src/main/java/sxy/apin/cards/uncommon/AllWaters.java
@@ -1,43 +1,37 @@
-package sxy.apin.cards.common;
+package sxy.apin.cards.uncommon;
 
 import basemod.abstracts.CustomCard;
-import com.megacrit.cardcrawl.actions.common.HealAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import sxy.apin.character.Furina;
 import sxy.apin.helper.FurinaHelper;
 
 import static sxy.apin.character.Furina.Enums.FURINA_BLUE;
 
-public class LaLettreAFocalors extends CustomCard {
-    public static final String ID = FurinaHelper.makeCardID(LaLettreAFocalors.class.getSimpleName());
-    private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID); // 从游戏系统读取本地化资源
+public class AllWaters extends CustomCard {
+    public static final String ID = FurinaHelper.makeCardID(AllWaters.class.getSimpleName());
+    private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
     private static final String NAME = CARD_STRINGS.NAME;
     private static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION;
     private static final String IMG_PATH = "sxy/apin/img/cards/Strike.png";
-    private static final int COST = 0;
+    private static final int COST = 1;
     private static final CardType TYPE = CardType.SKILL;
     private static final CardColor COLOR = FURINA_BLUE;
-    private static final CardRarity RARITY = CardRarity.COMMON;
-    // 是否指向敌人
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
     private static final CardTarget TARGET = CardTarget.SELF;
-    private int count = 1;
 
-    public LaLettreAFocalors() {
+    public AllWaters() {
         super(ID, NAME, IMG_PATH, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
-        this.baseHeal = 2;
-        this.tags.add(CardTags.HEALING);
+        this.magicNumber = 3;
+        this.baseMagicNumber = this.magicNumber;
     }
 
     @Override
     public void upgrade() {
         if (!this.upgraded) {
             this.upgradeName();
-            this.count = 2;
-            this.baseHeal = 3;
+            this.upgradeMagicNumber(2);
         }
         this.rawDescription = CARD_STRINGS.UPGRADE_DESCRIPTION;
         this.initializeDescription();
@@ -45,12 +39,6 @@ public class LaLettreAFocalors extends CustomCard {
 
     @Override
     public void use(AbstractPlayer abstractPlayer, AbstractMonster abstractMonster) {
-        for (int i = 0; i < this.count; i++) {
-            int extraHealth = Furina.getRevelry();
-            AbstractDungeon.actionManager.addToBottom(
-                    new HealAction(abstractPlayer, abstractPlayer, this.baseHeal + extraHealth)
-            );
-            Furina.consumeRevelry(1);
-        }
+        // TODO
     }
 }
--- src/main/java/sxy/apin/cards/uncommon/LaLettreAFocalors.java
+++ src/main/java/sxy/apin/cards/uncommon/LaLettreAFocalors.java
@@ -1,37 +1,43 @@
-package sxy.apin.cards.rare;
+package sxy.apin.cards.uncommon;
 
 import basemod.abstracts.CustomCard;
+import com.megacrit.cardcrawl.actions.common.HealAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import sxy.apin.character.Furina;
 import sxy.apin.helper.FurinaHelper;
 
 import static sxy.apin.character.Furina.Enums.FURINA_BLUE;
 
-public class AllWaters extends CustomCard {
-    public static final String ID = FurinaHelper.makeCardID(AllWaters.class.getSimpleName());
-    private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
+public class LaLettreAFocalors extends CustomCard {
+    public static final String ID = FurinaHelper.makeCardID(LaLettreAFocalors.class.getSimpleName());
+    private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID); // 从游戏系统读取本地化资源
     private static final String NAME = CARD_STRINGS.NAME;
     private static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION;
     private static final String IMG_PATH = "sxy/apin/img/cards/Strike.png";
-    private static final int COST = 1;
+    private static final int COST = 0;
     private static final CardType TYPE = CardType.SKILL;
     private static final CardColor COLOR = FURINA_BLUE;
-    private static final CardRarity RARITY = CardRarity.RARE;
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    // 是否指向敌人
     private static final CardTarget TARGET = CardTarget.SELF;
+    private int count = 1;
 
-    public AllWaters() {
+    public LaLettreAFocalors() {
         super(ID, NAME, IMG_PATH, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
-        this.magicNumber = 3;
-        this.baseMagicNumber = this.magicNumber;
+        this.baseHeal = 2;
+        this.tags.add(CardTags.HEALING);
     }
 
     @Override
     public void upgrade() {
         if (!this.upgraded) {
             this.upgradeName();
-            this.upgradeMagicNumber(2);
+            this.count = 2;
+            this.baseHeal = 3;
         }
         this.rawDescription = CARD_STRINGS.UPGRADE_DESCRIPTION;
         this.initializeDescription();
@@ -39,6 +45,12 @@ public class AllWaters extends CustomCard {
 
     @Override
     public void use(AbstractPlayer abstractPlayer, AbstractMonster abstractMonster) {
-        // TODO
+        for (int i = 0; i < this.count; i++) {
+            int extraHealth = Furina.getRevelry();
+            AbstractDungeon.actionManager.addToBottom(
+                    new HealAction(abstractPlayer, abstractPlayer, this.baseHeal + extraHealth)
+            );
+            Furina.consumeRevelry(1);
+        }
     }
 }
