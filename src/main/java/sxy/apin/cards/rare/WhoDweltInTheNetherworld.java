b82bbdb5a8abe368db844e5a774fe63e6e7bef5d
Apin <qjksxy@163.com>
2024-09-18 21:59:16 +0800

    :pencil: ~缝缝补补~ 平衡性调整

--- src/main/java/sxy/apin/cards/rare/WhoDweltInTheNetherworld.java
+++ src/main/java/sxy/apin/cards/rare/WhoDweltInTheNetherworld.java
@@ -13,7 +13,7 @@ import sxy.apin.power.WhoDweltInTheNetherworldPower;
 import static sxy.apin.character.Furina.Enums.FURINA_BLUE;
 
 /**
- * 若非处幽冥  命之座4  孤心沙龙提供的元素能量+1/2。
+ * 若非处幽冥  命之座4  消耗 。回合开始阶段获得 !M! 点 furina_mod:元素能量 。
  */
 public class WhoDweltInTheNetherworld extends CustomCard {
     public static final String ID = FurinaHelper.makeCardID(WhoDweltInTheNetherworld.class.getSimpleName());
@@ -30,12 +30,15 @@ public class WhoDweltInTheNetherworld extends CustomCard {
     public WhoDweltInTheNetherworld() {
         super(ID, NAME, IMG_PATH, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
         this.exhaust = true;
+        this.baseMagicNumber = 2;
+        this.magicNumber = 2;
     }
 
     @Override
     public void upgrade() {
         if (!this.upgraded) {
             this.upgradeName();
+            this.upgradeMagicNumber(1);
         }
         this.rawDescription = CARD_STRINGS.UPGRADE_DESCRIPTION;
         this.initializeDescription();
@@ -43,18 +46,9 @@ public class WhoDweltInTheNetherworld extends CustomCard {
 
     @Override
     public void use(AbstractPlayer abstractPlayer, AbstractMonster abstractMonster) {
-        if (FurinaHelper.hasPower(WhoDweltInTheNetherworldPower.POWER_ID)) {
-            if (!this.upgraded) {
-                return;
-            } else {
-                WhoDweltInTheNetherworldPower power = (WhoDweltInTheNetherworldPower) FurinaHelper.getPower(WhoDweltInTheNetherworldPower.POWER_ID);
-                assert power != null;
-                power.upgrade();
-            }
-        }
         AbstractDungeon.actionManager.addToBottom(
                 new ApplyPowerAction(abstractPlayer, abstractPlayer,
-                        new WhoDweltInTheNetherworldPower(abstractPlayer, 1, upgraded), 0)
+                        new WhoDweltInTheNetherworldPower(abstractPlayer, this.magicNumber), 0)
         );
     }
 }
